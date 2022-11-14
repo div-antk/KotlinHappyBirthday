e5fe9c32e19d9cf53134daa6ceb783afceb3d749
div-antk <51283468+owaii@users.noreply.github.com>
2022-11-15 02:06:35 +0900

    画像を読み込む

--- app/src/main/java/com/example/kotlinhappybirthday/MainActivity.kt
+++ app/src/main/java/com/example/kotlinhappybirthday/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.kotlinhappybirthday.ui.theme.KotlinHappyBirthdayTheme
@@ -54,10 +55,20 @@ fun BirthdayGreetingWithText(message: String, from: String) {
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
+@Preview(showBackground = false)
 @Composable
 fun BirthdayCardPreview() {
     KotlinHappyBirthdayTheme {
-        BirthdayGreetingWithText("HPD James", "- from Emma")
+        BirthdayGreetingWithImage("HPD James", "- from Emma")
     }
 }
