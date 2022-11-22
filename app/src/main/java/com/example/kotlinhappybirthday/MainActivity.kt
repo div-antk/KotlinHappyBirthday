c56b1c742d3404950ac472dba55aeddded822338
div-antk <51283468+owaii@users.noreply.github.com>
2022-11-22 23:59:00 +0900

    textのリソース管理を変更

--- app/src/main/java/com/example/kotlinhappybirthday/MainActivity.kt
+++ app/src/main/java/com/example/kotlinhappybirthday/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -90,6 +91,6 @@ fun BirthdayGreetingWithImage(message: String, from: String) {
 @Composable
 fun BirthdayCardPreview() {
     KotlinHappyBirthdayTheme {
-        BirthdayGreetingWithImage("HPD James", "- from Emma")
+        BirthdayGreetingWithImage(stringResource(R.string.hbd_text), stringResource(R.string.signature_text))
     }
 }
